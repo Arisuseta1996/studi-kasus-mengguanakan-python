f99924d1e6c26d74561902650c435a13a5ea8f73
dtxg <100848749+dtxg@users.noreply.github.com>
2022-03-04 13:41:44 +0700

    Update and rename angkot4.py to angkot5.py
    hay saya ingin menambahkan index angkot 3 ltidak ber operasi

--- angkot5.py
+++ angkot5.py
@@ -2,13 +2,14 @@
 # i 4 dan 10 lembur
 #  lainya rusak
 # menggunakan if else
+# menambahkan 3 lagi rusak
 
 jmlAngkot = 10;
 angBeroperasi = 6;
 
 for noAngkot in range(jmlAngkot):
     noAngkot += 1
-    if (noAngkot <= angBeroperasi)&(noAngkot != 4):
+    if (noAngkot <= angBeroperasi)&(noAngkot != 4)&(noAngkot!=3):
         print(f'Angkot No. {noAngkot} beroperasi dengan baik')
     elif (noAngkot == 10)|(noAngkot == 4):
         print(f'Angkot No. {noAngkot} Lembur')
